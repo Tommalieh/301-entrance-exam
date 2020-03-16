bd332646fd1548c102e7d91127aa58b190696876
Tommalieh <muhammedtommalieh@gmail.com>
2020-03-16 14:53:09 +0200

    Solved Question 3

---
+++ js/todo.js
@@ -0,0 +1,79 @@
+'use strict'
+
+
+
+var container;
+var ulEl;
+var tasksString;
+
+var tasks = [];
+function Task(task, date) {
+    this.task = task;
+    this.date = date;
+    tasks.push(this);
+}
+
+
+if (!localStorage.getItem('Tasks')){
+    console.log('empty')
+}
+
+else{
+    tasksString = localStorage.getItem('Tasks');
+    tasks = JSON.parse(tasksString);
+    render();
+}
+
+
+var form = document.getElementById('taskForm');
+form.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    console.log(event);
+    var inputTask = event.target.task.value;
+    console.log(inputTask);
+    var inputDate = event.target.date.value;
+    console.log(inputDate);
+
+    new Task(inputTask, inputDate);
+
+    var stringTasks = JSON.stringify(tasks);
+    localStorage.setItem('Tasks', stringTasks);
+
+    render();
+});
+
+
+function render() {
+
+    console.log(typeof(ulEl));
+
+    if (typeof(ulEl) == 'object') {
+
+        ulEl.remove();
+
+        }
+
+        container = document.getElementById('tasks');
+        ulEl = document.createElement('ul');
+        container.appendChild(ulEl);
+
+        for (var i = 0; i < tasks.length; i++) {
+
+            var liEl1 = document.createElement('li');
+            liEl1.setAttribute('class', 'taskStyle');
+            liEl1.textContent = `${i+1}. ${tasks[i].task}`;
+            ulEl.appendChild(liEl1);
+
+            var spanEl = document.createElement('span');
+            spanEl.setAttribute('class', 'spanStyle');
+            spanEl.textContent = 'X';
+            ulEl.appendChild(spanEl);
+
+            var liEl2 = document.createElement('li');
+            liEl2.setAttribute('class', 'dateStyle');
+            liEl2.textContent = tasks[i].date;
+            ulEl.appendChild(liEl2);
+
+    }
+}
